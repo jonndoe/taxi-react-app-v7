736beb0136cc99d8faaaceaf3191f32deb52737a
jonndoe <qb89@yandex.ru>
2020-10-14 03:26:08 +0300

    added cors settings in settings.py file

--- server/taxi/taxi/settings.py
+++ server/taxi/taxi/settings.py
@@ -39,12 +39,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.postgres',
     'django.contrib.staticfiles',
+    'corsheaders',
     'channels',
     'rest_framework',
     'trips',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -157,3 +159,7 @@ SIMPLE_JWT = {
     'REFRESH_TOKEN_LIFETIME': datetime.timedelta(days=1),
     'USER_ID_CLAIM': 'id',
 }
+
+CORS_ORIGIN_WHITELIST = [
+    "http://localhost:8080",
+]
