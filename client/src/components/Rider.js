66774c268e30f1ae69dc312d611e36b9404c7073
jonndoe <qb89@yandex.ru>
2020-10-14 17:30:22 +0300

    added TripMedia component

--- client/src/components/Rider.js
+++ client/src/components/Rider.js
@@ -1,17 +1,60 @@
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   Breadcrumb, Card, Col, Row
 } from 'react-bootstrap';
 
 import { Redirect } from 'react-router-dom';
+
+import TripCard from './TripCard';
 import { isRider } from "../services/AuthService";
+import { getTrips } from "../services/TripService";
 
 function Rider (props) {
+
+  const [trips, setTrips] = useState([])
+
+  //The useEffect() function takes two arguments. The first argument is a
+  // function that gets executed whenever the Rider component refreshes.
+  // The second argument is a list of variables that React should monitor.
+  // When the monitored code changes, it should trigger React to reload
+  // the useEffect() function. Passing an empty list ensures that the useEffect()
+  // function only runs once.
+  useEffect(() => {
+    const loadTrips = async () => {
+      const { response, isError } = await getTrips();
+      if (isError) {
+        setTrips([]);
+      } else {
+        setTrips(response.data);
+      }
+    }
+    loadTrips();
+  }, []);
 
   if (!isRider()) {
       return <Redirect to='/' />
   }
+
+
+  const getCurrentTrips = () => {
+    return trips.filter(trip => {
+      return (
+        trip.driver !== null &&
+        trip.status !== 'REQUESTED' &&
+        trip.status !== 'COMPLETED' &&
+        trip.status !== 'CANCELLED'
+      );
+    });
+  };
+
+  const getCompletedTrips = () => {
+    return trips.filter(trip => {
+      return trip.status === 'COMPLETED';
+    });
+  };
+
+
 
   return (
     <Row>
